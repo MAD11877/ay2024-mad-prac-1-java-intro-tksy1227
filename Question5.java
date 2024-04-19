276427de86b1e5d62aebf8f94354ef8f4567071e
chloe <149999780+tksy1227@users.noreply.github.com>
2024-04-19 13:23:24 +0800

    question5

--- Question5.java
+++ Question5.java
@@ -27,6 +27,32 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
-    
+    System.out.print("Enter number of integers to be entered: ");
+    int n = in.nextInt();
+
+    // Create arrays to store the integers and their frequencies
+    int[] numbers = new int[n];
+    int[] frequencies = new int[101]; // Assuming integers are between 0 and 100
+
+    // Loop to get the integers from the user and update the frequencies
+    for (int i = 0; i < n; i++) {
+        System.out.print("Enter an integer: ");
+        int num = in.nextInt();
+        numbers[i] = num;
+        frequencies[num]++;
+    }
+
+    // Find the mode (number with the highest frequency)
+    int mode = 0;
+    int maxFrequency = 0;
+    for (int i = 0; i < frequencies.length; i++) {
+        if (frequencies[i] > maxFrequency) {
+            mode = i;
+            maxFrequency = frequencies[i];
+        }
+    }
+
+    // Print the mode
+    System.out.println("The mode is: " + mode);
   }
 }
