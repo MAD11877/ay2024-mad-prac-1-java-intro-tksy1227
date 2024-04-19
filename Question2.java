b6739defc89c085de826108001233d3bc85f9b05
chloe <149999780+tksy1227@users.noreply.github.com>
2024-04-19 14:19:05 +0800

    question2

--- Question2.java
+++ Question2.java
@@ -1,17 +1,15 @@
 import java.util.Scanner;
 
-public class Question2
-{
-  public static void main(String[] args)
-  {
+public class Question2 {
+  public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     System.out.print("Height in cm:");
-    double height = (in.nextDouble()/100);
+    double height = (in.nextDouble() / 100);
     System.out.print("Weight in kg:");
     double weight = in.nextDouble();
 
     double bmi = weight / (height * height);
 
-    System.out.println("BMI: "+bmi);
+    System.out.println(bmi); // Output only the BMI value
   }
 }
